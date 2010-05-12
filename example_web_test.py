b428253fa34437b5962c00695e85578ae3035763
U-JMATHES-LAPTOP\jmathes <jmathes@jmathes-laptop.(none)>
2010-05-11 20:39:03 -0800

    maybe this will help synch

--- example_web_test.py
+++ example_web_test.py
@@ -7,7 +7,7 @@ class UrlTest(ts_test_framework.TeamStoriesTest):
 		
 	def test_rootEmitsHelloWorld(self):
 		self.web_get("") #empty string should map to http://localhost:8080/
-		self.assertDocumentContains("Hello world!!")
+		self.assertDocumentContains("Team Stories")
 		
 if __name__ == '__main__':
     unittest.main()

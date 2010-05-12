2d80ec4e746a50cdf057189682ec2ee2b3a72b78
DFectuoso <santiago1717@gmail.com>
2010-05-11 20:03:37 -0700

    Adding /story/id with template

--- main.py
+++ main.py
@@ -9,12 +9,16 @@ class NewHandler(webapp.RequestHandler):
     def get(self):
         self.response.out.write(template.render('templates/new.html', locals()))
 
+class StoryHandler(webapp.RequestHandler):
+    def get(self,story_id):
+        self.response.out.write(template.render('templates/story.html', locals()))
+
 def main():
     application = webapp.WSGIApplication([('/', MainHandler),
-                                          ('/new', NewHandler)
+                                          ('/new', NewHandler),
+                                          ('/story/(.+)', StoryHandler),
                                          ], debug=True)
     util.run_wsgi_app(application)
-
 
 if __name__ == '__main__':
     main()
